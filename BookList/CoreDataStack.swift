bdcf5261b19b2b2ce715a66496862faff16d817c
YutaGoto <goto@rejob.co.jp>
2016-04-29 18:38:26 +0900

    add booklist model

--- BookList/CoreDataStack.swift
+++ BookList/CoreDataStack.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    let context:NSManagedObjectContext
+    
+    let appDocumentDirURL: NSURL = {
+        let fileManager = NSFileManager.defaultManager()
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains:.UserDomainMask)
+        
+        return urls.last!
+    }()
+    
+    init() {
+        let bundle = NSBundle.mainBundle()
+        guard let modelURL = bundle.URLForResource("BookListModel", withExtension: "momd") else {
+            fatalError()
+        }
+        
+        guard let model = NSManagedObjectModel(contentsOfURL: modelURL) else {
+            fatalError()
+        }
+        
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        
+        context = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        context.persistentStoreCoordinator = coordinator
+    }
+    
+    func addPersistentStoreWithCompletionHandler(completionHandler: (()->Void)?) {
+        
+        let backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
+        
+        dispatch_async(backgroundQueue) {
+            let dirURL = self.appDocumentDirURL
+            let storeURL = dirURL.URLByAppendingPathComponent("BookList.sqlite")
+            
+            do {
+                let coordinator = self.context.persistentStoreCoordinator!
+                try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: storeURL, options: nil)
+                completionHandler?()
+            } catch let error as NSError {
+                fatalError("\(error)")
+            }
+        }
+    }
+    
+    func saveContext() throws {
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                print("\(error)")
+                throw error
+            }
+        }
+    }
+    
+}
